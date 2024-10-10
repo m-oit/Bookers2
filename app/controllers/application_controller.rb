465eb0d76ef52af66dc8871764cca4f09063d106
EC2 Default User <177226299+m-oit@users.noreply.github.com>
2024-10-10 08:29:16 +0000

    [中断]Bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,13 @@ def after_sign_in_path_for(resource)
     about_path
 end
 
+def after_sign_out_path_for(resource)
+    about_path
+end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
-
