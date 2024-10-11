4f611c57ca33f570574a9f909212c769973047eb
EC2 Default User <177226299+m-oit@users.noreply.github.com>
2024-10-11 08:05:41 +0000

    [update]画像投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
   get 'homes/top'
   devise_for :users
   root to: "homes#top"
+  
+  resources :books, only: [:new, :create, :index, :show]
+  
   get 'homes/about' => 'homes#about',as: 'about'
 end
