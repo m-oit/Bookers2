f6718433112bfdff486645853067cfc9fce32d0c
EC2 Default User <177226299+m-oit@users.noreply.github.com>
2024-10-16 01:51:45 +0000

    [update]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
   get 'homes/top'
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
   
-  resources :books, only: [:new, :create, :index, :show]
-  
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
   get 'homes/about' => 'homes#about',as: 'about'
 end
