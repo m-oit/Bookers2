fb3a01d7437e4c89b0be3a6f71d4dd64eba3839c
EC2 Default User <177226299+m-oit@users.noreply.github.com>
2024-10-18 04:51:56 +0000

    [update]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+
+ def index
+   @users = User.all
+   @books = Book.all.page(params[:page])
+ end
+  
+  
  def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page])  
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
-  get 'homes/top'
   root to: "homes#top"
   devise_for :users
   
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
-  resources :users, only: [:show, :edit, :update]
-  get 'homes/about' => 'homes#about',as: 'about'
+  resources :users, only: [:index, :show, :edit, :update]
+  get 'home/about' => 'homes#about',as: 'about'
 end
